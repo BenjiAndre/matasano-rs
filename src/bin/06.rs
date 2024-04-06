350d6165de267870126357179e7eedda66fbc649
Benjamin Andre <benjaminandre23@gmail.com>
2024-04-06 10:01:34 +0900

    started work on challenge 6

--- src/bin/06.rs
+++ src/bin/06.rs
@@ -1,13 +1,22 @@
-fn main() {
-    todo!();
+use matasano_rs::base64;
+
+const INPUT: &str = include_str!("../../inputs/06.in");
+
+fn hamming_distance(s1: &str, s2: &str) -> usize {
+    s1.as_bytes()
+        .iter()
+        .zip(s2.as_bytes())
+        .fold(0, |acc, (&b1, &b2)| acc + (b1 ^ b2).count_ones() as usize)
 }
+
+fn main() {}
 
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
-    fn it_works() {
-        todo!();
+    fn test_hamming_distance() {
+        assert_eq!(37, hamming_distance("this is a test", "wokka wokka!!!"));
     }
 }
