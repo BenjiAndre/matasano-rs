ef95fcf02dddbfe12a2d76f0f17626a3ba6c159b
Benjamin Andre <benjaminandre23@gmail.com>
2024-04-06 09:43:27 +0900

    finished challenge 5

--- src/bin/05.rs
+++ src/bin/05.rs
@@ -1,13 +1,21 @@
+use matasano_rs::bytes_to_hex;
+
+const PLAINTEXT: &[u8] =
+    b"Burning 'em, if you ain't quick and nimble\nI go crazy when I hear a cymbal";
+const CIPHERTEXT: &str = "0b3637272a2b2e63622c2e69692a23693a2a3c6324202d623d63343c2a26226324272765272a282b2f20430a652e2c652a3124333a653e2b2027630c692b20283165286326302e27282f";
+
 fn main() {
-    todo!();
-}
+    let xored_plaintext = b"ICE"
+        .iter()
+        .cycle()
+        .zip(PLAINTEXT)
+        .map(|(key, c)| key ^ c)
+        .collect::<Vec<_>>();
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+    let ciphertext = bytes_to_hex(&xored_plaintext);
 
-    #[test]
-    fn it_works() {
-        todo!();
-    }
+    println!("Result Ciphertext : {}", ciphertext);
+    println!("Expected Ciphertext: {}", CIPHERTEXT);
+    assert_eq!(ciphertext, CIPHERTEXT);
+    println!("\x1b[32mCORRECT\x1b[0m");
 }
