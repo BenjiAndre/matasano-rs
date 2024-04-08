ee4d9cf28aaa918b28a6cfc7b932d7e94dd61140
Benjamin Andre <benjaminandre23@gmail.com>
2024-04-08 18:30:16 +0900

    finished challenge 7

--- src/bin/07.rs
+++ src/bin/07.rs
@@ -1,13 +1,12 @@
+use itertools::Itertools;
+use matasano_rs::base64::decode;
+use openssl::symm::{decrypt, Cipher};
+
+const CIPHERTEXT: &str = include_str!("../../inputs/07.in");
+const KEY: &[u8] = b"YELLOW SUBMARINE";
+
 fn main() {
-    todo!();
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        todo!();
-    }
+    let bytes = decode(&CIPHERTEXT.lines().join(""));
+    let plaintext = decrypt(Cipher::aes_128_ecb(), KEY, None, &bytes).unwrap();
+    println!("{}", String::from_utf8(plaintext).unwrap())
 }
