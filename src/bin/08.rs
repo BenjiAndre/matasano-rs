c396908c6b44c81aae27f2add960e3bb1a132832
Benjamin Andre <benjaminandre23@gmail.com>
2024-04-09 09:25:14 +0900

    finished challenge 8

--- src/bin/08.rs
+++ src/bin/08.rs
@@ -1,13 +1,36 @@
+use itertools::Itertools;
+
+const INPUT: &str = include_str!("../../inputs/08.in");
+
 fn main() {
-    todo!();
-}
+    let (number, ciphertext) = INPUT
+        .lines()
+        .find_position(|text| {
+            text.as_bytes()
+                .chunks(32)
+                .tuple_combinations()
+                .any(|(b1, b2)| b1 == b2)
+        })
+        .unwrap();
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+    let matching_encryption = ciphertext
+        .as_bytes()
+        .chunks(32)
+        .tuple_combinations()
+        .find(|(b1, b2)| b1 == b2)
+        .unwrap()
+        .0
+        .to_vec();
 
-    #[test]
-    fn it_works() {
-        todo!();
+    println!("The ciphertext n = {} was encrypted using ECB", number);
+    for block in ciphertext.as_bytes().chunks(32) {
+        if block == matching_encryption {
+            println!(
+                "\x1b[32m{}\x1b[0m",
+                String::from_utf8(block.to_vec()).unwrap()
+            );
+        } else {
+            println!("{}", String::from_utf8(block.to_vec()).unwrap());
+        }
     }
 }
