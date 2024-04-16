c45002d1017e0d8bc1f931451a7e06eeda3c2dbd
Benjamin Andre <benjaminandre23@gmail.com>
2024-04-16 14:37:02 +0900

    started challenge 13

--- src/bin/13.rs
+++ src/bin/13.rs
@@ -1,13 +1,22 @@
-fn main() {
-    todo!();
+use core::panic;
+use std::collections::HashMap;
+
+fn parse(input: &str) -> HashMap<String, String> {
+    let mut profile = HashMap::new();
+    for pair in input.split('&') {
+        let (key, value) = pair.split_once('=').unwrap();
+        profile.insert(key.to_string(), value.to_string());
+    }
+    profile
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        todo!();
+fn profile_for(input: &str) -> String {
+    if input.contains('=') || input.contains('&') {
+        panic!("making code less cancer");
     }
+    format!("email={}&uid=10&role=user", input)
+}
+
+fn main() {
+    todo!();
 }
